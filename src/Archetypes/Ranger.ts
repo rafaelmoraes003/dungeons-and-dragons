db042b42361bcf5e8b40880432b95be7e7798bab
rafael moraes <rafaelsm003@gmail.com>
2022-09-22 16:15:42 -0300

    feat: cria classe Ranger que estende da classe Archetype e implementa seus atributos e métodos

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,21 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Ranger extends Archetype {
+  private static rangerCreatedInstances = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  public static createdArchetypeInstances(): number {
+    Ranger.rangerCreatedInstances += 1;
+    return Ranger.rangerCreatedInstances;
+  }
+}
