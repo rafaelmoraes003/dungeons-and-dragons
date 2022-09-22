e4d4bafbdfd7e17cafc6b02178116befa170faab
rafael moraes <rafaelsm003@gmail.com>
2022-09-22 15:12:22 -0300

    feat: cria classe Halfling que estende da classe Race e implementa seus atributos e métodos

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,21 @@
+import RacesMaxPoints from './enumRacesMaxPoints';
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private static _halflingCreatedInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = RacesMaxPoints.Halfling;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    Halfling._halflingCreatedInstances += 1;
+    return Halfling._halflingCreatedInstances;
+  }
+}
