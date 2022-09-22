1e38abd41d8062d7c7a16a8f46e4e47a25959833
rafael moraes <rafaelsm003@gmail.com>
2022-09-22 15:11:53 -0300

    feat: cria classe Dwarf que estende da classe Race e implementa seus atributos e métodos

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,21 @@
+import RacesMaxPoints from './enumRacesMaxPoints';
+import Race from './Race';
+
+export default class Dwarf extends Race {
+  private static _dwarfCreatedInstances = 0;
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = RacesMaxPoints.Dwarf;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  public static createdRacesInstances(): number {
+    Dwarf._dwarfCreatedInstances += 1;
+    return Dwarf._dwarfCreatedInstances;
+  }
+}
