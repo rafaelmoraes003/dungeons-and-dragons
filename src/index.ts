fbcdb2ddd2f659174732ab19370ad2c376ef6470
rafael moraes <rafaelsm003@gmail.com>
2022-09-22 23:56:37 -0300

    feat: cria algumas instâncias das classes criadas anteriormente

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+/// //////////////////////////////////////////
+
+const player1 = new Character('Mario');
+const player2 = new Character('Luigi');
+const player3 = new Character('Toad');
+
+player1.levelUp();
+player1.levelUp();
+
+/// //////////////////////////////////////////
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+/// //////////////////////////////////////////
+
+const pvp = new PVP(player2, player3);
+
+/// //////////////////////////////////////////
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+/// //////////////////////////////////////////
+
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => battle.fight());
+};
+
+/// //////////////////////////////////////////
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
